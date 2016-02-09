eeb8a6d0642903a09b840ca872653d7f17e79f16
U-TRINITY-HEALTH\hdgd8104 <hdgd8104@HQILL607040.hq.trinity-health.org>
2016-02-09 16:00:40 -0500

    clean it up

--- 2016-03/kraig-lightning-talk/get-Quandle-data.R
+++ 2016-03/kraig-lightning-talk/get-Quandle-data.R
@@ -5,14 +5,19 @@
 #devtools::install_github('quandl/R-package')
 
 library(Quandl)
-library(ggplot2)
 library(magrittr)
-library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(AnomalyDetection)
+library(BreakoutDetection)
+
+#sessionInfo()
 
 #Quandl.api_key("yourauthenticationtoken") 
-Quandl.api_key("5nUk_6XSenkDnvK2N2-w")
 
-msft_data <- Quandl("GOOG/NASDAQ_MSFT", collapse = "daily")[, c(1,5)]
+msft_data <- Quandl("GOOG/NASDAQ_MSFT", collapse = "daily")[, c(1,5)] # closing price
+msft_data$Date <- ymd(msft_data$Date)
+colnames(msft_data) <- c("timestamp", "count")
 
 # specific date range
 #start_date = "yyyy-mm-dd", end_date = "yyyy-mm-dd"
@@ -25,16 +30,24 @@ msft_data <- Quandl("GOOG/NASDAQ_MSFT", collapse = "daily")[, c(1,5)]
 #ploting example
 
 msft_data %>% 
-  ggplot(aes(x = Date, y = Close)) +
+  ggplot(aes(x = timestamp, y = count)) +
   geom_line(color = "#FAB521") +
   theme(panel.background = element_rect(fill = "#393939"),
         panel.grid.major.x = element_blank(),
         panel.grid.major.y = element_line(colour = "white", size = 0.1),
-        panel.grid.minor = element_line(colour = "white", size = 0.1)) +
+        panel.grid.minor = element_line(colour = "white", size = 0.1)
+  ) +
   xlab("Date") + 
   ylab("Closing Price") + 
-  ggtitle("MSFT") 
+  ggtitle("MSFT")
 
+msft_vec <- msft_data$count[order(msft_data$timestamp)]
 
+outliers <- AnomalyDetectionVec(msft_vec, direction = 'both', plot = T, period = 7, longterm_period = 56)
+outliers$plot
+
+changes <- breakout(msft_vec, method = 'multi', plot = T, min.size = 7)
+changes$plot
+changes$loc
 
 
